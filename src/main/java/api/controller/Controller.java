7b8d0e5090c1784c22fcc6f93907413a00cafbc0
Ajinkya Jadhav <ajinkyajadhav789789@gmail.com>
2023-03-08 13:18:22 +0530

    changed method names

--- src/main/java/api/controller/Controller.java
+++ src/main/java/api/controller/Controller.java
@@ -1,11 +1,16 @@
 package api.controller;
 
+import java.util.List;
+
 import api.modalclass.Modal;
 import api.services.Service_CLS;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class Controller {
@@ -13,36 +18,78 @@ public class Controller {
     private Service_CLS serviceCls;
 
     @PostMapping("/addtask")
-    public Modal addTask(@RequestBody Modal sc) {
-        Modal modal = this.serviceCls.addToList(sc);
-        return modal;
+    public ResponseEntity<Modal> addTask(@RequestBody Modal sc) {
+        boolean isPresent = false;
+        int b = sc.getTaskId();
+        try {
+            isPresent = this.serviceCls.addToList(sc);
+            if (isPresent == true) {
+                return new ResponseEntity(" Successfully Added New Data", HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity("This (" + b + ") Id Is Already Exist ", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity("Some Error Occurred", HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/deletealltask")
-    public List<Modal> deleteAll() {
-        return this.serviceCls.deleteAll();
-
+    public ResponseEntity<List<Modal>> deleteAll() {
+        List<Modal> lm = this.serviceCls.deleteAll();
+        if (lm.size() == 0) {
+            return new ResponseEntity("All Tasks Are Deleted", HttpStatus.ACCEPTED);
+        }
+        return ResponseEntity.of(Optional.of(lm));
     }
 
     @DeleteMapping("/deletetask/{id}")
-    public void deleteSingle(@PathVariable("id") int id) {
-        this.serviceCls.deleteSingle(id);
+    public ResponseEntity<Void> deleteSingle(@PathVariable("id") int id) {
+        try {
+            boolean h = this.serviceCls.deleteSingle(id);
+            if (h == true) {
+                return new ResponseEntity("Deleted Successfully Data Of Id :) " + id, HttpStatus.OK);
+            } else {
+                return new ResponseEntity("This (" + id + ") Id Is Not Present :( ", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity("Unexpected Error Occurred :( ", HttpStatus.BAD_GATEWAY);
+        }
     }
 
-
     @PutMapping("/updatetask/{id}")
-    public Modal update(@RequestBody Modal entcls, @PathVariable("id") int id) {
-        this.serviceCls.update(entcls, id);
-        return entcls;
+    public ResponseEntity<Modal> update(@RequestBody Modal entcls, @PathVariable("id") int id) {
+        try {
+            int a = this.serviceCls.update(entcls, id);
+            if (a == 1) {
+                return new ResponseEntity("This Id Already Exist Plz Use Different Task Id :( " , HttpStatus.NOT_ACCEPTABLE);
+            } else if (a == 2) {
+                return new ResponseEntity("Successfully Updated Data Of Id :) " + id, HttpStatus.FOUND);
+            } else {
+                return new ResponseEntity("This (" + id + ") Id Is Not Present :( ", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity("Unexpected Error Occurred Update :( ", HttpStatus.NO_CONTENT);
+        }
     }
 
     @GetMapping("/showalltask")
-    public List<Modal> showAll() {
-        return this.serviceCls.getDetails();
+    public ResponseEntity<List<Modal>> showAll() {
+        List<Modal> data = this.serviceCls.getDetails();
+        if (data.size() <= 0) {
+            return new ResponseEntity("No Data Found :( ", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.of(Optional.of(data));
     }
 
     @GetMapping("/showtask/{id}")
-    public Modal showSingle(@PathVariable("id") int id) {
-        return this.serviceCls.getSingle(id);
+    public ResponseEntity<Modal> showSingle(@PathVariable("id") int id) {
+        Modal m = this.serviceCls.getSingle(id);
+        if (m == null) {
+            return new ResponseEntity("This (" + id + ") Id Is Not Available :( ", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.of(Optional.of(m));
     }
 }
--- src/main/java/api/services/Service_CLS.java
+++ src/main/java/api/services/Service_CLS.java
@@ -12,8 +12,9 @@ public class Service_CLS {
     private static List<Modal> list1 = new ArrayList<Modal>();
 
     static {
-        list1.add(new Modal(01, "walk", 5, "go for walk ", "in progress"));
-        list1.add(new Modal(02, "talk", 5, "go for walk ", "in progress"));
+        list1.add(new Modal(01, "learn go", 5, "learning ", "in progress"));
+        list1.add(new Modal(02, "learn c", 4, "not started yet ", "pending"));
+        list1.add(new Modal(03, "learn c++", 3, "not started yet ", "pending"));
     }
 
     public List<Modal> getDetails() {
@@ -22,13 +23,23 @@ public class Service_CLS {
 
     public Modal getSingle(int id) {
         Modal modal = null;
-        modal = list1.stream().filter(e -> e.getTaskId() == id).findFirst().get();
+        try {
+            modal = list1.stream().filter(e -> e.getTaskId() == id).findFirst().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return modal;
     }
 
-    public Modal addToList(Modal modal) {
+    public boolean addToList(Modal modal) {
+        int a = modal.getTaskId();
+        boolean Present = checkId(a);
+        if (Present) {
+            return false;
+        }
+        list1 = list1.stream().filter(i -> i.getTaskId() != a).collect(Collectors.toList());
         list1.add(modal);
-        return modal;
+        return true;
     }
 
     public List<Modal> deleteAll() {
@@ -36,20 +47,68 @@ public class Service_CLS {
         return list1;
     }
 
-    public void deleteSingle(int id) {
-        list1 = list1.stream().filter(i -> i.getTaskId() != id).collect(Collectors.toList());
+    public boolean deleteSingle(int id) {
+        boolean isThere = false;
+        boolean Present = checkId(id);
+        if (Present) {
+            list1 = list1.stream().filter(i -> i.getTaskId() != id).collect(Collectors.toList());
+            isThere = true;
+        }
+        return isThere;
     }
 
-    public void update(Modal entcls, int upid) {
-        list1 = list1.stream().map(i -> {
-            if (i.getTaskId() == upid) {
-                i.setTaskId(entcls.getTaskId());
-                i.setTaskName(entcls.getTaskName());
-                i.setPriority(entcls.getPriority());
-                i.setDescription(entcls.getDescription());
-                i.setProgress(entcls.getProgress());
+    public int update(Modal entcls, int upid) {
+        boolean isThere = false;
+        int ForOutP = 0;
+        int JSid = entcls.getTaskId(); //it will store data of updating id (incoming JSON format)
+        boolean Present = checkId(upid); //it is used to store id is existed in db
+        //1
+        if (Present) {
+            if (upid != JSid) {
+                isThere = false;
+            } //user enter unique id or same id
+            if (upid == JSid) {
+                isThere = true;
             }
-            return i;
-        }).collect(Collectors.toList());
+            if (isThere == false) { //it is used to when user enter data which is existed
+                for (Modal nn : list1) {
+                    if (nn.getTaskId() == JSid) {
+                        ForOutP = 1;
+                        return ForOutP;
+                    } else {
+                        isThere = true;
+                    }
+                }
+            }
+        }
+        //2
+        if (isThere) {
+            list1 = list1.stream().map(i -> {
+                if (i.getTaskId() == upid) {
+                    i.setTaskId(entcls.getTaskId());
+                    i.setTaskName(entcls.getTaskName());
+                    i.setPriority(entcls.getPriority());
+                    i.setDescription(entcls.getDescription());
+                    i.setProgress(entcls.getProgress());
+                }
+                return i;
+            }).collect(Collectors.toList());
+            ForOutP = 2;
+            return ForOutP;
+        }
+        return ForOutP;
+    }
+
+    public boolean checkId(int id) {
+        boolean b = false;
+        for (Modal v : list1) {
+            if (v.getTaskId() == id) {
+                b = true;
+                return b;
+            } else {
+                b = false;
+            }
+        }
+        return b;
     }
 }
